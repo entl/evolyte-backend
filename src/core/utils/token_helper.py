6b1d854ce5ad15f810313422719e8fc887994ce1
entl <maxvorobyov01@gmail.com>
2025-02-21 21:43:27 +0000

    feat: prepare auth utils

---
+++ src/core/utils/token_helper.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+
+import jwt
+
+from src.settings import settings
+from src.core.exceptions.token import DecodeTokenException, ExpiredTokenException
+
+
+class TokenHelper:
+    @staticmethod
+    def encode(payload: dict, expire_period: int = 3600) -> str:
+        token = jwt.encode(
+            payload={
+                **payload,
+                "exp": datetime.utcnow() + timedelta(seconds=expire_period),
+            },
+            key=settings.jwt_secret_key,
+            algorithm=settings.jwt_algorithm,
+        )
+        return token
+
+    @staticmethod
+    def decode(token: str) -> dict:
+        try:
+            return jwt.decode(
+                token,
+                settings.jwt_secret_key,
+                settings.jwt_algorithm,
+            )
+        except jwt.exceptions.DecodeError:
+            raise DecodeTokenException
+        except jwt.exceptions.ExpiredSignatureError:
+            raise ExpiredTokenException
+
+    @staticmethod
+    def decode_expired_token(token: str) -> dict:
+        try:
+            return jwt.decode(
+                token,
+                settings.jwt_secret_key,
+                settings.jwt_algorithm,
+                options={"verify_exp": False},
+            )
+        except jwt.exceptions.DecodeError:
+            raise DecodeTokenException

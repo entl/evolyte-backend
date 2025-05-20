358d09e519afb3f2300b2ce2c63aff9418ededfc
entl <maxvorobyov01@gmail.com>
2025-05-20 22:02:05 +0100

    feat: create dep for google provider

--- src/auth/google/routers.py
+++ src/auth/google/routers.py
@@ -1,10 +1,8 @@
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter
 from starlette import status
 
-from src.auth.schemas import VerifyTokenRequest, RefreshTokenRequest, TokenPairResponse
-from src.core.dependencies.auth import AuthServiceDep
 from src.auth.google.service import GoogleAuthProvider
-from src.core.dependencies.db import UowDep
+from src.core.dependencies.auth import GoogleAuthServiceDep
 from .schemas import GoogleOAuth2Response
 
 
@@ -12,8 +10,7 @@ google_auth_router = APIRouter(prefix="/auth/google", tags=["Google Auth"])
 
 
 @google_auth_router.post("/login", status_code=status.HTTP_200_OK)
-async def auth_user(oauth_info: GoogleOAuth2Response, uow: UowDep, auth_service: AuthServiceDep):
-    google_auth = GoogleAuthProvider(uow=uow, auth_service=auth_service)
-    token_pair = google_auth.authenticate(oauth_info=oauth_info)
+async def auth_user(oauth_info: GoogleOAuth2Response, google_auth_service: GoogleAuthServiceDep):
+    token_pair = google_auth_service.authenticate(oauth_info=oauth_info)
 
     return token_pair
--- src/core/dependencies/auth.py
+++ src/core/dependencies/auth.py
@@ -5,6 +5,7 @@ from fastapi import Depends
 
 from .db import UowDep
 from src.auth.service import AuthService
+from src.auth.google.service import GoogleAuthProvider
 
 
 def auth_service(uow: UowDep):
@@ -12,3 +13,10 @@ def auth_service(uow: UowDep):
 
 
 AuthServiceDep = Annotated[AuthService, Depends(auth_service)]
+
+
+def google_auth_service(uow: UowDep, auth_service: AuthServiceDep):
+    return GoogleAuthProvider(uow=uow, auth_service=auth_service)
+
+
+GoogleAuthServiceDep = Annotated[GoogleAuthProvider, Depends(google_auth_service)]

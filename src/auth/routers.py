98337496baccf1493ac12ab7de53dac41c579593
entl <maxvorobyov01@gmail.com>
2025-05-20 21:44:21 +0100

    fix: update response schemas

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,33 +1,20 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Request, Response, HTTPException
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 
-from .schemas import TokenResponse, VerifyTokenRequest
+from .schemas import VerifyTokenRequest, RefreshTokenRequest, TokenPairResponse
 from src.core.dependencies.auth import AuthServiceDep
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
-@auth_router.post("/refresh", response_model=TokenResponse)
-def refresh_token(request: Request, response: Response, auth_service: AuthServiceDep):
-    refresh_token = request.cookies.get("refresh_token")
-    if not refresh_token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Refresh token not found in cookies")
+@auth_router.post("/refresh", response_model=TokenPairResponse)
+def refresh_token(request: RefreshTokenRequest, auth_service: AuthServiceDep):
+    access_token = auth_service.refresh_access_token(refresh_token=request.refresh_token)
 
-    token_pair = auth_service.refresh_tokens(refresh_token=refresh_token)
-
-    response.set_cookie(
-        key="refresh_token",
-        value=token_pair.refresh_token,
-        httponly=True,
-        secure=True,
-        samesite="lax",
-        max_age=60 * 60 * 24 * 30,  # 30 days
-    )
-
-    return TokenResponse(access_token=token_pair.access_token)
+    return TokenPairResponse(access_token=access_token, refresh_token=request.refresh_token)
 
 
 @auth_router.post("/verify", status_code=status.HTTP_200_OK)
@@ -36,21 +23,11 @@ def verify_token(request: VerifyTokenRequest, auth_service: AuthServiceDep):
     return {"valid": True}
 
 
-@auth_router.post("/login", response_model=TokenResponse, status_code=status.HTTP_200_OK)
+@auth_router.post("/login", response_model=TokenPairResponse, status_code=status.HTTP_200_OK)
 def login(
-    response: Response,
     user_credentials: Annotated[OAuth2PasswordRequestForm, Depends()],
     auth_service: AuthServiceDep,
 ):
     token_pair = auth_service.login(email=user_credentials.username, password=user_credentials.password)
 
-    response.set_cookie(
-        key="refresh_token",
-        value=token_pair.refresh_token,
-        httponly=True,
-        secure=True,
-        samesite="lax",
-        max_age=60 * 60 * 24 * 30,  # 30 days
-    )
-
-    return TokenResponse(access_token=token_pair.access_token)
+    return TokenPairResponse(access_token=token_pair.access_token, refresh_token=token_pair.refresh_token)

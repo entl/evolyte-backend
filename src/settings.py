83929589fad6217bd51bf2d90a565876a9fa27a5
entl <maxvorobyov01@gmail.com>
2025-06-02 21:22:00 +0100

    feat: add envs for redis and enode api

--- src/settings.py
+++ src/settings.py
@@ -8,6 +8,13 @@ class Settings(BaseSettings):
     pg_database_name: str
     pg_database_username: str
 
+    redis_host: str
+    redis_port: str
+    redis_password: str
+    redis_cache_db: str
+    redis_celery_broker_db: str
+    redis_celery_backend_db: str
+
     jwt_secret_key: str
     jwt_algorithm: str
     jwt_token_expiration_time: int  # in seconds
@@ -17,6 +24,11 @@ class Settings(BaseSettings):
 
     google_client_id: str
     google_client_secret: str
+
+    enode_client_id: str
+    enode_client_secret: str
+    enode_api_url: str
+    enode_oauth_url: str
 
     cors_origins: list[str]
 

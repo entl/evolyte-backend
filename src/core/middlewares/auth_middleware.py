0cfa474f9d7b4e61cc05c7b58df4d2551617c09f
entl <maxvorobyov01@gmail.com>
2025-07-16 20:20:35 +0100

    feat: rework auth to fetch id from headers set by api gateway

--- src/core/middlewares/auth_middleware.py
+++ src/core/middlewares/auth_middleware.py
@@ -9,32 +9,50 @@ from starlette.requests import HTTPConnection
 from src.core.exceptions.token import TokenException
 from src.core.utils.token_helper import TokenHelper
 from src.schemas import CurrentUser
+from src.user.schemas import UserRoles
+
+
+# class AuthBackend(AuthenticationBackend):
+#     async def authenticate(self, conn: HTTPConnection) -> Tuple[bool, CurrentUser]:
+#         current_user = CurrentUser()
+#         authorization: str | None = conn.headers.get("Authorization")
+#         if not authorization:
+#             return False, current_user
+
+#         try:
+#             token_type, payload_encoded = authorization.split(" ")
+#             if token_type.lower() != "bearer":
+#                 return False, current_user
+#         except ValueError:
+#             return False, current_user
+
+#         if not payload_encoded:
+#             return False, current_user
+
+#         try:
+#             payload = TokenHelper.decode(payload_encoded)
+#             user_id = payload.get("user_id")
+#         except TokenException:
+#             return False, current_user
+
+#         current_user.id = user_id
+#         return True, current_user
 
 
 class AuthBackend(AuthenticationBackend):
     async def authenticate(self, conn: HTTPConnection) -> Tuple[bool, CurrentUser]:
         current_user = CurrentUser()
-        authorization: str | None = conn.headers.get("Authorization")
-        if not authorization:
+        user_id: str | None = conn.headers.get("X-User-ID")
+        role: UserRoles | str | None = conn.headers.get("X-User-Role")
+        if not user_id:
             return False, current_user
-
-        try:
-            token_type, payload_encoded = authorization.split(" ")
-            if token_type.lower() != "bearer":
-                return False, current_user
-        except ValueError:
+        
+        if not role:
             return False, current_user
-
-        if not payload_encoded:
-            return False, current_user
-
-        try:
-            payload = TokenHelper.decode(payload_encoded)
-            user_id = payload.get("user_id")
-        except TokenException:
-            return False, current_user
-
+        
         current_user.id = user_id
+        current_user.role = role
+
         return True, current_user
 
 
--- src/schemas.py
+++ src/schemas.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel, Field
 
+from src.user.schemas import UserRoles
+
 
 class CurrentUser(BaseModel):
     id: int = Field(None, description="ID")
+    role: str = Field(None, description="User role")
     permissions: list[str] = Field(None, description="Permissions")
 
     class Config:
